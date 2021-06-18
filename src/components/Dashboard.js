9c9274fa96913c3ec22dbac3d47a96daa7a2b5fe
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-06-17 23:14:02 -0400

    adding web sockets

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import axios from "axios";
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
 
 import Loading from "components/Loading";
 import Panel from "components/Panel";
@@ -34,6 +41,16 @@ class Dashboard extends Component {
         interviewers: interviewers.data,
       });
     });
+    this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    this.socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        this.setState((previousState) =>
+          setInterview(previousState, data.id, data.interview)
+        );
+      }
+    };
 
     if (focused) {
       this.setState({ focused });
@@ -45,6 +62,10 @@ class Dashboard extends Component {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
   }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
   render() {
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused,
@@ -54,22 +75,22 @@ class Dashboard extends Component {
       {
         id: 1,
         label: "Total Interviews",
-        value: 6,
+        getValue: getTotalInterviews,
       },
       {
         id: 2,
         label: "Least Popular Time Slot",
-        value: "1pm",
+        getValue: getLeastPopularTimeSlot,
       },
       {
         id: 3,
         label: "Most Popular Day",
-        value: "Wednesday",
+        getValue: getMostPopularDay,
       },
       {
         id: 4,
         label: "Interviews Per Day",
-        value: "2.3",
+        getValue: getInterviewsPerDay,
       },
     ];
 
@@ -87,7 +108,7 @@ class Dashboard extends Component {
           key={item.id}
           id={item.id}
           label={item.label}
-          value={item.value}
+          value={item.getValue(this.state)}
           onSelect={(e) => this.selectPanel(item.id)}
         />
       ));
